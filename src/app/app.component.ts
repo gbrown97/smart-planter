ac29a333a7e1030e34d40ce7892a4ffd95be0c66
gbrown97 <brown.1366@wright.edu>
2021-03-28 17:27:29 -0400

    Basic dynamic card list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,34 +30,21 @@ export class AppComponent {
     maxMoisture: 10
   }
 
-  tempResult: PlantDefinition = {
-    name: "Plant",
-    type: "Generic Type",
-    minTemp: 70,
-    maxTemp: 80,
-    sunlightLevel: "Medium",
-    minMoisture: 1,
-    maxMoisture: 10
-  }
-
   plantList: Array<PlantDefinition> = [];
 
   constructor(public dialog: MatDialog) {}
 
   addPlant() {
-    console.log(this.plantList);
     const dialogRef = this.dialog.open(AddPlantDialogComponent, {
       width: '250px',
       data: this.plantDef
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log("Result" + result);
       if(result != '' || result != undefined){
         let cloneData = JSON.parse(JSON.stringify(result));
         this.plantList.push(cloneData);
       }
-      console.log(this.plantList);
     });
   }
 }
